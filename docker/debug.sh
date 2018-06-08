9f31b69fd8853e29da9dd4f37dc7ff090497150c
sullof <sullof@gmail.com>
2018-06-07 18:57:20 -0700

    Fixing docker configuration

--- docker/debug.sh
+++ docker/debug.sh
@@ -6,14 +6,14 @@ docker rm app-tweedentity-app-debug
 docker stop tweedentity-app
 docker rm tweedentity-app
 
-source docker/.default.env && docker run -it \
+docker run -it \
   --name tweedentity-app-debug \
   --link tweedentity-redis:redis \
   -p 9095 \
   -v $PWD:/usr/src/app \
   -v /vol/log/tweedentity_app:/var/log/tweedentity_app \
-  -e VIRTUAL_HOST=tweedentity.com,www.tweedentity.com \
-  -e LETSENCRYPT_HOST=tweedentity.com,www.tweedentity.com \
+  -e VIRTUAL_HOST=app.tweedentity.com \
+  -e LETSENCRYPT_HOST=app.tweedentity.com \
   -e LETSENCRYPT_EMAIL=admin@tweedentity.com \
   -w /usr/src/app node:6 npm run start
 
